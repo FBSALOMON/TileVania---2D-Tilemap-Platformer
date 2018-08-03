ae38ba576cef76af5a6dfc495a0f74e1ac276d6e
FBSALOMON <fbsalomon@gmail.com>
2018-08-02 23:50:08 -0400

    TileVania - 2D Tilemap Platformer(32/35)

---
+++ TileVania - 2D Tilemap Platformer/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed = 1f;
+
+    Rigidbody2D myRigidbody2D;
+	// Use this for initialization
+	void Start () {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myRigidbody2D.velocity = new Vector2(moveSpeed, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+              
+	}
+
+    bool isFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FlipEnemyDirection();
+    }
+
+    private void FlipEnemyDirection()
+    {
+        myRigidbody2D.velocity = new Vector2(-myRigidbody2D.velocity.x, 0f);
+        transform.localScale = new Vector2(-transform.localScale.x, 1f);
+    }  
+}
---
+++ TileVania - 2D Tilemap Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    //Config
+    [SerializeField]
+    public float runSpeed = 5f;
+    [SerializeField]
+    public float jumpPower = 5f;
+    [SerializeField]
+    public float climbSpeed = 5f;
+    [SerializeField]
+    public Vector2 deathKick;
+
+    // State
+    bool isAlive = true;
+
+    //Cached component references
+    private Rigidbody2D myRigidbody2D;
+    private Animator myAnimator;
+    private CapsuleCollider2D myBodyCollider;
+    private BoxCollider2D myFeet;
+    private float gravityScaleAtStart;
+
+    //Message then methods
+    void Start () {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody2D.gravityScale;
+	}
+
+	void Update ()
+    {
+        if (!isAlive) { return; }
+        
+        Run();
+        Jump();
+        FlipSprite();
+        ClimbLadder();
+        Die();
+
+
+    }
+
+    private void Run()
+    {
+        float controlThrown = Input.GetAxis("Horizontal") * runSpeed;
+        Vector2 playerVelocity = new Vector2(controlThrown, myRigidbody2D.velocity.y);
+        myRigidbody2D.velocity = playerVelocity;
+
+        bool PlayerIsBeingMoved = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", PlayerIsBeingMoved);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+        }
+    }
+
+    private void Jump()
+    {
+        bool isPlayerOnTheGround = (myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")));
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && isPlayerOnTheGround)
+        {
+            Vector2 playerVelocity = new Vector2(0f, jumpPower);
+            myRigidbody2D.velocity += playerVelocity;
+        }
+    }
+
+    private void ClimbLadder()
+    {
+        if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+            myRigidbody2D.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("Climbing", false);
+            return; }
+
+        float controlThrown = CrossPlatformInputManager.GetAxis("Vertical");            
+        Vector2 climbVelocity = new Vector2(myRigidbody2D.velocity.x, controlThrown * climbSpeed);
+        myRigidbody2D.velocity = climbVelocity;
+
+        myRigidbody2D.gravityScale = 0;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody2D.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+
+    }
+
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
+        {
+            myAnimator.SetTrigger("Die");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            isAlive = false;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+}
